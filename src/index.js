8e963649381f5decedd73f6dbd38956b04d9c6ec
ashnajay <ashna.j.kumar.24@dartmouth.edu>
2023-04-12 17:59:13 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let count = 0;
+setInterval(() => {
+  count += 1;
+  $('#main').html('You have been on this page for ' + count + ' seconds');
+}, 1000);
