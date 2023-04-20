f2b3ad0c94b56b74b960c6876bff6243885e7da5
ashnajay <ashna.j.kumar.24@dartmouth.edu>
2023-04-20 14:02:32 -0400

    Fixed test-id error

--- src/index.jsx
+++ src/index.jsx
@@ -1,46 +1,45 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable react/function-component-definition */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import { createRoot, useParams } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './style.scss';
-import {
-  BrowserRouter, Routes, Route, NavLink,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink, useParams } from 'react-router-dom';
 
-function About(props) {
+const About = (props) => {
   return <div> All there is to know about me </div>;
-}
-function Welcome(props) {
+};
+const Welcome = (props) => {
   return <div>Welcome</div>;
-}
+};
+const Test = (props) => {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+};
 
-function Nav(props) {
+const Nav = (props) => {
   return (
     <nav>
       <ul>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+
       </ul>
     </nav>
   );
-}
-
-function Test(props) {
-  const { id } = useParams();
-  return <div> ID: {id} </div>;
-}
+};
 
 function FallBack(props) {
   return <div>URL Not Found</div>;
 }
 
-function App(props) {
+const App = (props) => {
   return (
     <BrowserRouter>
       <div>
-        <Nav>
-          <li><NavLink to="/test/id1">test id1</NavLink></li>
-          <li><NavLink to="/test/id2">test id2</NavLink></li>
-        </Nav>
+        <Nav />
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/about" element={<About />} />
@@ -50,6 +49,7 @@ function App(props) {
       </div>
     </BrowserRouter>
   );
-}
+};
+
 const root = createRoot(document.getElementById('main'));
 root.render(<App />);
