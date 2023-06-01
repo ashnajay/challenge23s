df78d25051f2b5dfbd31cabb9faaf06548083b88
ashnajay <ashna.j.kumar.24@dartmouth.edu>
2023-05-31 20:42:12 -0400

    Removed redux skeleton

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import './style.scss';
-
-let count = 0;
-setInterval(() => {
-  count += 1;
-  $('#main').html('You have been on this page for ' + count + ' seconds');
-}, 1000);
