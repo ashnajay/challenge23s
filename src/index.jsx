c7b3ef766a82528b8bc9f247cf48b0acec1e49ba
ashnajay <ashna.j.kumar.24@dartmouth.edu>
2023-05-31 21:40:46 -0400

    Clean up

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
 
 import App from './components/app';
 
